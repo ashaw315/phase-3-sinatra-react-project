8f518158ff05f96de2736f46229e511878562035
Adam <ashaw315@gmail.com>
2022-01-24 17:26:53 -0500

    added routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/listings" do
+    Listing.all.to_json
+  end
+
+  get "/users" do
+    User.all.to_json
+  end
+
+  get "/reviews" do
+    Review.all.to_json
   end
 
 end
